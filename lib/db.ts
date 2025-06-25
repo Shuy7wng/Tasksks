ca923d330cf52ded5c4f4fef0b88bbe99da48975
Shuy7wng <giuliawang07@gmail.com>
2025-06-25 15:43:44 +0200

    ,kjhgc

--- lib/db.ts
+++ lib/db.ts
@@ -1,15 +1,9 @@
 import mysql from "mysql2/promise";
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL environment variable not set");
-}
-
-const url = new URL(process.env.DATABASE_URL);
-
 export const db = mysql.createPool({
-  host: url.hostname,
-  user: url.username,
-  password: url.password,
-  database: url.pathname.slice(1),
-  port: url.port ? parseInt(url.port) : 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT ? +process.env.DB_PORT : 3306,
 });
