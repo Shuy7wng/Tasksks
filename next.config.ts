f22fbafbb0ba38ed4b39403b1c2e32ad738ad026
Shuy7wng <giuliawang07@gmail.com>
2025-06-25 10:47:57 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  // altre configurazioni qui
 };
 
 export default nextConfig;
